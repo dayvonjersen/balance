6526aae98ce0d3e3acc456c844910d091f366063
tso <tso@teknik.io>
2017-11-05 16:11:10 -0500

    improve stylesheet, add plus/minus radio button

--- balance.go
+++ balance.go
@@ -1,8 +1,7 @@
 /*
 	TODO:
-	- better design (css)
 	- floating point errors (±1 cent)
-	- deposit/withdrawal as radio button
+	- table overflow: auto, scroll to bottom
 */
 package main
 
@@ -95,9 +94,14 @@ func main() {
 			date := time.Now()
 			desc := r.PostForm["desc"][0]
 			amt := r.PostForm["amt"][0]
+			plusminus := r.PostForm["plusminus"][0]
 
 			amtFloat, _ := strconv.ParseFloat(amt, 64)
 			amount := int64(amtFloat * 100)
+
+			if plusminus == "1" {
+				amount *= -1
+			}
 
 			if amount != 0 {
 				register = append(register, &registerEntry{date, desc, amount})
@@ -115,37 +119,45 @@ func main() {
 		}
 
 		io.WriteString(w, `
+		<title>Transaction Register</title>
 		<link rel='stylesheet' href='/public/style.css'>
 		<form action="/" method="post">
 			<fieldset>
 				<legend>new entry</legend>
-				<div>
-					<label>date</label>
-					<input type="text" name="date" disabled value="(now)">
+				<div class='row'>
+					<div>
+						<label>date</label>
+						<input type="text" name="date" disabled value="(now)">
+					</div>
+					<div>
+						<label>description</label>
+						<input autofocus type="text" name="desc" value="">
+					</div>
+					<div>
+						<label>amount</label>
+						<div class='amount'>
+							<label><input type="radio" name="plusminus" value="0">+</label>
+							<label><input type="radio" name="plusminus" value="1" checked>-</label>
+							<input type="text" name="amt" value="">
+						</div>
+					</div>
 				</div>
-				<div>
-					<label>description</label>
-					<input type="text" name="desc" value="">
+				<div class='row'>
+					<div>
+						<input class="primary" type="submit" value="enter">
+					</div>
 				</div>
-				<div>
-					<label>amount</label>
-					<input type="text" name="amt" value="">
-				</div>
-				<input class="primary" type="submit" value="enter">
 			</fieldset>
 		</form>
-		<hr>
 		<table>
 		<thead>
 			<tr class="primary">
-				<th>&nbsp;</th>
-				<th class='left mono'>date</th>
-				<th class='center mono'>transaction</th>
-				<th class='right mono'>amount</th>
+				<th width='33%' colspan='2' class='left mono'>date</th>
+				<th width='33%' class='center mono'>transaction</th>
+				<th width='33%' class='right mono'>amount</th>
 			</tr>
 		</thead>
 		<tbody>`)
-
 		var total int64 = 0
 		for i, entry := range register {
 			amt := entry.Amount
@@ -186,9 +198,14 @@ func main() {
 		fmt.Fprintf(w, `
 		</tbody>
 		<tfoot>
-			<tr class="primary">
-				<th colspan=3 class='right mono'>total</th>
-				<th class='right mono'>%s%d.%02d</th>
+			<tr>
+				<th colspan='3'></th>
+				<th class='primary'>
+					<div class='mono total'>
+						<span>total:</span>
+						<span>%s%d.%02d</span>
+					</div>
+				</th>
 			</tr>
 		</tfoot>
 		</table>`,
